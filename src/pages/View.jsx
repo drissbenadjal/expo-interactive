b300bdd82ad264de587ba3d60f614558ff6e57f4
DRISS BENADJAL <driss.benadjal@edu.univ-eiffel.fr>
2023-03-17 19:17:39 +0100

    add sphere and shader material

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -3,9 +3,10 @@ import { Loader } from '../components/CrossHair/Loader/Loader'
 
 import React, { Suspense, useState, useRef, useEffect } from 'react'
 import { Canvas, useFrame, useLoader, extend, createRoot, events } from '@react-three/fiber'
-import { useGLTF, PointerLockControls, OrbitControls } from '@react-three/drei'
+import { useGLTF, PointerLockControls, shaderMaterial, Sphere } from '@react-three/drei'
 import { VRButton, XR, Controllers, Hands, Interactive, RayGrab, useXR } from '@react-three/xr'
 import * as THREE from 'three'
+import glsl from 'babel-plugin-glsl/macro'
 
 import { CrossHair } from '../components/CrossHair/CrossHair'
 
@@ -43,15 +44,59 @@ export const View = () => {
     useEffect(() => {
         const audioambiance = new Audio('../assets/sounds/test.mp3')
         //quand la souris a bouger on play le son
-        window.addEventListener('mousemove', () => {
+        window.addEventListener('click', () => {
             audioambiance.play()
             audioambiance.volume = 0.2
         })
     }, [])
     const audiotest = new Audio('../assets/sounds/ds.mp3')
 
+    const ColorShiftMaterial = shaderMaterial(
+        { uTime: 0, uColorStart: new THREE.Color('lightBlue'), uColorEnd: new THREE.Color('white') },
+        glsl`
+        varying vec2 vUv;
+        void main() {
+            vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+            vec4 viewPosition = viewMatrix * modelPosition;
+            vec4 projectionPosition = projectionMatrix * viewPosition;
+            gl_Position = projectionPosition;
+            vUv = uv;
+        }`,
+        glsl`
+        #pragma glslify: cnoise3 = require(glsl-noise/classic/3d.glsl) 
+        uniform float uTime;
+        uniform vec3 uColorStart;
+        uniform vec3 uColorEnd;
+        varying vec2 vUv;
+        void main() {
+            vec2 displacedUv = vUv + cnoise3(vec3(vUv * 7.0, uTime * 0.1));
+            float strength = cnoise3(vec3(displacedUv * 5.0, uTime * 0.2));
+            float outerGlow = distance(vUv, vec2(0.5)) * 4.0 - 1.4;
+            strength += outerGlow;
+            strength += step(-0.2, strength) * 0.8;
+            strength = clamp(strength, 0.0, 1.0);
+            vec3 color = mix(uColorStart, uColorEnd, strength);
+            gl_FragColor = vec4(color, 1.0);
+        }`,
+    )
+
+    // declaratively
+    extend({ ColorShiftMaterial })
+
     //utiliser pointerlockcontrols
     extend({ PointerLockControls })
+
+    function Model(props) {
+        const mymaterial = useRef(null);
+        useFrame((state, delta) => { (mymaterial.current.uTime += delta) })
+        return (
+            <mesh>
+                <Sphere position={[0, 1.3, 4]} scale={1}>
+                    <colorShiftMaterial ref={mymaterial} time={1} />
+                </Sphere>
+            </mesh>
+        )
+    }
 
     return (
         <>
@@ -60,7 +105,6 @@ export const View = () => {
             <VRButton />
             <Canvas
                 gl={{ antialias: true }}
-                //zoomer la camera
                 camera={{ position: [0, 1.5, 0], fov: 70, rotation: [0, 0, 0] }}
                 onCreated={({ gl }) => {
                     gl.shadowMap.enabled = true
@@ -82,6 +126,7 @@ export const View = () => {
                     <boxGeometry />
                     <directionalLight castShadow position={[1, 2, 3]} intensity={2} />
                     <ambientLight intensity={0.5} />
+                    <Model />
                     <primitive
                         object={gallery.scene}
                         scale={1}
@@ -125,7 +170,6 @@ export const View = () => {
                                 onClick={(event) => {
                                     event.stopPropagation()
                                 }}
-
                             />
                         </RayGrab>
                     </Interactive>
