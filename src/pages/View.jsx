b499029179304e9e3a5c4433e2b133d02a7519bb
drissbenadjal <driss.benadjal@hotmail.com>
2023-03-23 16:08:54 +0100

    Update View.jsx

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -15,6 +15,9 @@ export const View = () => {
 
     const [loading, setLoading] = useState(true)
 
+    // const playBtn = useLoader(THREE.ImageBitmapLoader, './assets/images/play.png');
+    // console.log();
+
     const gallery = useGLTF(`./assets/modeles/vr_gallery/scene.gltf`);
     const laNuitEtoilee = useGLTF(`./assets/textures/LaNuitEtoilee.glb`);
     const soleilLevant = useGLTF(`./assets/textures/soleilLevant.glb`);
@@ -54,33 +57,11 @@ export const View = () => {
     extend({ PointerLockControls })
 
 
-    // Animation
-    //     const comp = useRef(); // create a ref for the root level element (for scoping)
-    //     const circle = useRef();
-
-    //     useLayoutEffect(() => {
-
-    //   // create our context. This function is invoked immediately and all GSAP animations and ScrollTriggers created during the execution of this function get recorded so we can revert() them later (cleanup)
-    //   let ctx = gsap.context(() => {
-
-    //     // Our animations can use selector text like ".box" 
-    //     // this will only select '.box' elements that are children of the component
-    //     gsap.to(".box", {});
-    //     // or we can use refs
-    //     gsap.to(circle.current, { rotation: 360 });
-
-    //   }, comp); // <- IMPORTANT! Scopes selector text
-
-    //   return () => ctx.revert(); // cleanup
-
-    // }, []); // <- empty dependency Array so it doesn't re-run on every render
-
-
 
     return (
         <>
             {/* <Suspense fallback={<h1>test</h1>}> */}
-            <Loader loading={loading} />
+            {/* <Loader loading={loading} /> */}
             <CrossHair />
             <VRButton />
             <Canvas
