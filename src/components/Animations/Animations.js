1e6ad686981a06f921cbda79eb892e33609a5233
DRISS BENADJAL <driss.benadjal@edu.univ-eiffel.fr>
2023-03-27 15:03:30 +0200

    test zoomVr

--- src/components/Animations/Animations.js
+++ src/components/Animations/Animations.js
@@ -7,3 +7,33 @@ export const zoom = (target, x, y, z) => {
 export const rotate = (target, x, y, z) => {
     gsap.to(target, { x: x, y: y, z: z })
 }
+
+// zoom animation in vanilla js without gsap with transition
+// export const zoomVR = (target, x, y, z) => {
+//     const duration = 500;
+//     //for three.js
+//     target.position.x = x;
+//     target.position.y = y;
+//     target.position.z = z;
+
+//     target.setAttribute('position', { x: x, y: y, z: z });
+//     target.setAttribute('animation', { property: 'position', to: { x: x, y: y, z: z }, dur: duration, easing: 'easeOutElastic' });
+// }
+
+export const zoomVR = (target, x, y, z) => {
+    const duration = 500;
+    const framesNumber = 60;
+
+    const xStep = (x - target.scene.position.x) / framesNumber;
+    const yStep = (y - target.scene.position.y) / framesNumber;
+    const zStep = (z - target.scene.position.z) / framesNumber;
+
+    setInterval(() => {
+        target.scene.position.x += xStep;
+        target.scene.position.y += yStep;
+        target.scene.position.z += zStep;
+    }, duration / framesNumber);
+}
+
+
+
--- src/components/Paint/Paint.jsx
+++ src/components/Paint/Paint.jsx
@@ -1,5 +1,5 @@
 import { Interactive, RayGrab } from '@react-three/xr';
-import { zoom, rotate } from '../Animations/Animations';
+import { zoom, rotate, zoomVR } from '../../utils/animation';
 
 
 export const Paint = ({ name, basePosition, baseRotation, hoverPosition, clickPosition, clickRotation, baseScale, paint }) => {
@@ -8,43 +8,44 @@ export const Paint = ({ name, basePosition, baseRotation, hoverPosition, clickPo
     return (
         <Interactive
             onHover={() => {
-                zoom(paint.scene.position, hoverPosition.x, hoverPosition.y, hoverPosition.z);
+                // zoom(paint.scene.position, hoverPosition.x, hoverPosition.y, hoverPosition.z);
+                zoomVR(paint, hoverPosition.x, hoverPosition.y, hoverPosition.z);
             }}
 
             onBlur={() => {
                 sound.pause()
-                zoom(paint.scene.position, basePosition.x, basePosition.y, basePosition.z);
-                rotate(paint.scene.rotation, baseRotation.x, baseRotation.y, baseRotation.z);
+                zoomVR(paint, basePosition.x, basePosition.y, basePosition.z);
+                // rotate(paint.scene.rotation, baseRotation.x, baseRotation.y, baseRotation.z);
             }}
 
             onSelect={() => {
-                rotate(paint.scene.rotation, clickRotation.x, clickRotation.y, clickRotation.z);
-                zoom(paint.scene.position, clickPosition.x, clickPosition.y, clickPosition.z);
+                // rotate(paint.scene.rotation, clickRotation.x, clickRotation.y, clickRotation.z);
+                zoomVR(paint, clickPosition.x, clickPosition.y, clickPosition.z);
                 sound.play()
             }}
         >
             {/* <RayGrab> */}
-                <primitive
-                    object={paint.scene}
-                    scale={baseScale}
-                    position={[basePosition.x, basePosition.y, basePosition.z]}
-                    rotation={[baseRotation.x, baseRotation.y, baseRotation.z]}
-                    onPointerOver={(event) => {
-                        zoom(paint.scene.position, hoverPosition.x, hoverPosition.y, hoverPosition.z);
-                    }}
-                    onPointerOut={(event) => {
-                        sound.pause()
-                        zoom(paint.scene.position, basePosition.x, basePosition.y, basePosition.z);
-                        rotate(paint.scene.rotation, baseRotation.x, baseRotation.y, baseRotation.z);
+            <primitive
+                object={paint.scene}
+                scale={baseScale}
+                position={[basePosition.x, basePosition.y, basePosition.z]}
+                rotation={[baseRotation.x, baseRotation.y, baseRotation.z]}
+                onPointerOver={(event) => {
+                    zoom(paint.scene.position, hoverPosition.x, hoverPosition.y, hoverPosition.z);
+                }}
+                onPointerOut={(event) => {
+                    sound.pause()
+                    zoom(paint.scene.position, basePosition.x, basePosition.y, basePosition.z);
+                    rotate(paint.scene.rotation, baseRotation.x, baseRotation.y, baseRotation.z);
 
-                    }}
-                    onClick={(event) => {
-                        rotate(paint.scene.rotation, clickRotation.x, clickRotation.y, clickRotation.z);
-                        zoom(paint.scene.position, clickPosition.x, clickPosition.y, clickPosition.z);
-                        sound.play()
-                        event.stopPropagation()
-                    }}
-                />
+                }}
+                onClick={(event) => {
+                    rotate(paint.scene.rotation, clickRotation.x, clickRotation.y, clickRotation.z);
+                    zoom(paint.scene.position, clickPosition.x, clickPosition.y, clickPosition.z);
+                    sound.play()
+                    event.stopPropagation()
+                }}
+            />
             {/* </RayGrab> */}
         </Interactive>
     )
