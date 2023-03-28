bd44c9ccbe13406ab6258a9a8aca31690080b710
Théo Grassien <theo.grassien@gmail.com>
2023-03-28 13:11:53 +0200

    add loader image

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,42 +1,44 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './Loader.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./Loader.css";
 
 export const Loader = ({ loading }) => {
+  const [display, setDisplay] = useState(true);
 
+  const loaderTopRef = useRef(null);
+  const loaderBottomRef = useRef(null);
 
-    const [display, setDisplay] = useState(true);
+  useEffect(() => {
+    if (!loading) {
+      // Faire une transition qui fait baisser la height du loader
+      for (let i = 0; i < 51; i++) {
+        setTimeout(() => {
+          loaderTopRef.current.style.height = `${50 - i}vh`;
+        }, i * 6);
+      }
+      for (let i = 0; i < 60; i++) {
+        setTimeout(() => {
+          loaderBottomRef.current.style.height = `${50 - i}vh`;
+        }, i * 6);
+      }
 
-    const loaderTopRef = useRef(null);
-    const loaderBottomRef = useRef(null);
-
-    useEffect(() => {
-        if (!loading) {
-            // Faire une transition qui fait baisser la height du loader
-            for (let i = 0; i < 51; i++) {
-                setTimeout(() => {
-                    loaderTopRef.current.style.height = `${50 - i}vh`;
-                }, i * 6);
-            }
-            for (let i = 0; i < 60; i++) {
-                setTimeout(() => {
-                    loaderBottomRef.current.style.height = `${50 - i}vh`;
-                }, i * 6);
-            }
-
-            setTimeout(() => {
-                setDisplay(false);
-            }, 500);
-        }
-    }, [loading]);
-
-    if (display) {
-        return (
-            <div className="loader">
-                <div className="loader__top" ref={loaderTopRef}></div>
-                <div className="loader__bottom" ref={loaderBottomRef}></div>
-            </div>
-        )
-    } else {
-        return null;
+      setTimeout(() => {
+        setDisplay(false);
+      }, 500);
     }
-}
+  }, [loading]);
+
+  if (display) {
+    return (
+      <div className="loader">
+        <div className="loader__top" ref={loaderTopRef}>
+          <img src="./assets/Images/Logo-top.webp" alt="" />
+        </div>
+        <div className="loader__bottom" ref={loaderBottomRef}>
+          <img src="./assets/Images/Logo-bottom.webp" alt="" />
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
